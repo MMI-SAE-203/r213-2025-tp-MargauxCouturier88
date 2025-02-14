915dd96ebea4978500376fa2c87d14aacc46a1aa
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2025-02-14 10:26:07 +0100

    Ajouter une offre

--- src/backend.mjs
+++ src/backend.mjs
@@ -35,3 +35,19 @@ export async function getSurface(s) {
     });
     return maisonSurface;
 }
+
+export async function addOffre(house) {
+    try {
+        await pb.collection('maison').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
+    }
+}
